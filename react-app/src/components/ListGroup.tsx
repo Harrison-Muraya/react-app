0bee855492aaf94297761d62190f2c4b89a40686
Harrison-Muraya <harrisonmuraya8@gmail.com>
2025-05-25 14:08:35 +0300

    Refactored ListGroup component to use a cities array and map function for rendering list items.

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,16 +1,31 @@
 
 function ListGroup() {
+    const cities = ["New York", "Los Angeles", "Chicago", "Houston", "Philadelphia"];
+
     return (
         <>
             <h1>List</h1>
             <ul className="list-group">
-                <li className="list-group-item">An item</li>
-                <li className="list-group-item">A second item</li>
-                <li className="list-group-item">A third item</li>
-                <li className="list-group-item">A fourth item</li>
-                <li className="list-group-item">And a fifth one</li>
+                {cities.map((city) => (<li className="list-group-item">{city}</li>))}
             </ul>
         </>
     )
 }
 export default ListGroup;
+
+
+// function ListGroup() {
+//     return (
+//         <>
+//             <h1>List</h1>
+//             <ul className="list-group">
+//                 <li className="list-group-item">An item</li>
+//                 <li className="list-group-item">A second item</li>
+//                 <li className="list-group-item">A third item</li>
+//                 <li className="list-group-item">A fourth item</li>
+//                 <li className="list-group-item">And a fifth one</li>
+//             </ul>
+//         </>
+//     )
+// }
+// export default ListGroup;
