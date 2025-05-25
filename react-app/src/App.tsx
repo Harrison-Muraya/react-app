efe2089dbdcf3eb1019a6f3854465b08c069d263
Harrison-Muraya <harrisonmuraya8@gmail.com>
2025-05-25 13:27:06 +0300

    Added ListGrop component and removed Message component from App.tsx

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,14 +1,24 @@
+import ListGrop from "./components/ListGroup"
+
+function App(){
+  return <div className=""><ListGrop/></div>
+}
+export default App;
+
+
+
 // import Message from "./message"
 // function App(){
 //   return <div className=""><Message /></div>
 // }
 // export default App;
 
-function Message (){
-  const name = 'harrison';
-  if(name){
-    return <h1>hello {name}</h1>
-  }
-    return <h1>hello world</h1>
-}
-export default Message;
+// function Message (){
+//   const name = 'harrison';
+//   if(name){
+//     return <h1>hello {name}</h1>
+//   }
+//     return <h1>hello world</h1>
+// }
+// export default Message;
+
